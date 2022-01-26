25a799c7eec75f811d316dfb9d652c7548b56fd9
Nam Khanh Nguyen <nguye2nk@mail.uc.edu>
2022-01-26 16:05:55 -0500

    Set view engine to ejs and get route to room.ejs

--- server.js
+++ server.js
@@ -3,6 +3,13 @@ const app = express()
 const server = require('http').Server(app)
 const io = require('socket.io').server
 const { v4: generateID } = require('uuid')
+
+app.set('view engine', 'ejs')
+
+app.get('/', (req, res) => {
+    res.render('room.ejs')
+})
+
 console.log('testing')
 
 server.listen(process.env.PORT || 3000)
