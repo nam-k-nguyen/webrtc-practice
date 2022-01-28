130f0bfb23a7ee2af4deae27382e88ddf12bbf71
Nam Khanh Nguyen <nguye2nk@mail.uc.edu>
2022-01-28 15:47:31 -0500

    update stun/turn servers

--- public/room.js
+++ public/room.js
@@ -3,7 +3,52 @@ const myPeer = new Peer(undefined, {
     host: '0.peerjs.com',
     port: '443', 
     debug: 3,
-    pingInterval: 5000
+    pingInterval: 5000,
+    iceServers: [
+        { urls: 'stun:stun.l.google.com:19302'  }, 
+        { urls: 'stun:stun1.l.google.com:19302' }, 
+        { urls: 'stun:stun2.l.google.com:19302' },
+        {
+            url: 'turn:numb.viagenie.ca',
+            credential: 'muazkh',
+            username: 'webrtc@live.com'
+        },
+        {
+            url: 'turn:192.158.29.39:3478?transport=udp',
+            credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
+            username: '28224511:1379330808'
+        },
+        {
+            url: 'turn:192.158.29.39:3478?transport=tcp',
+            credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
+            username: '28224511:1379330808'
+        },
+        {
+            url: 'turn:turn.bistri.com:80',
+            credential: 'homeo',
+            username: 'homeo'
+        },
+        {
+            url: 'turn:turn.anyfirewall.com:443?transport=tcp',
+            credential: 'webrtc',
+            username: 'webrtc'
+        },
+        {
+            url: "turn:13.250.13.83:3478?transport=udp",
+            username: "YzYNCouZM1mhqhmseWk6",
+            credential: "YzYNCouZM1mhqhmseWk6"
+        },
+        {
+            url: 'turn:relay.backups.cz',
+            credential: 'webrtc',
+            username: 'webrtc'
+        },
+        {
+            url: 'turn:relay.backups.cz?transport=tcp',
+            credential: 'webrtc',
+            username: 'webrtc'
+        },
+    ]
 })
 
 
