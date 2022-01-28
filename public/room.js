6bd521d61a4581c288a0cd8ccdf8ff5b37fb7dd3
Nam Khanh Nguyen <nguye2nk@mail.uc.edu>
2022-01-28 02:06:02 -0500

    Add lines

--- public/room.js
+++ public/room.js
@@ -1,1 +1,120 @@
 const socket = io('/')
+const myPeer = new Peer(undefined, {
+    secure: true,
+    host: '0.peerjs.com',
+    port: '443'
+})
+
+
+let myName
+let myId
+let myStream
+const videoGrid = document.querySelector('#video-grid')
+const videoList = []
+const peers = {
+    // id: {
+    //     video: "video element",
+    //     call: "call"
+    // }
+}
+
+
+// Run when user receive id from peer server
+myPeer.on('open', userId => {
+    b()
+    a("ID received from peerjs")
+    myId = userId
+    mediaStreaming()
+    socket.emit('join-room', ROOM_ID, myId)
+})
+
+
+// Run when called by existing user
+myPeer.on('call', call => {
+    call.answer(myStream)
+    
+    b()
+    a('Called by existing user')
+
+    call.on('stream', existingUserStream => {
+        
+        b()
+        a('Stream sent by existing user')
+        a(existingUserStream)
+        
+        const existingUserVideo = createVideo()
+        if (!peers[call.peer]) {
+        }
+        addVideoStream(existingUserVideo, existingUserStream)
+
+        peers[call.peer] = {
+            video: existingUserVideo,
+            call: call
+        }
+    })
+
+})
+
+
+// run when other user connected
+socket.on('user-connected', connectedUserId => {
+    const call = myPeer.call(connectedUserId, myStream)
+    
+    b()
+    a('Called new user')
+
+    call.on('stream', connectedUserStream => {
+
+        b()
+        a('Stream sent by connected new user')
+        a(connectedUserStream)
+
+        const connectedUserVideo = createVideo()
+        if (!peers[connectedUserId]) {
+        }
+        addVideoStream(connectedUserVideo, connectedUserStream)
+
+        peers[connectedUserId] = {
+            video: connectedUserVideo,
+            call: call
+        }
+    })
+
+    call.on('close', () => {
+
+    })
+})
+
+// run when other user disconnected
+socket.on('user-disconnected', disconnectedUserId => {/* FIX HERE */ })
+
+
+// ----------------------
+
+
+// Function to request media stream from user
+function mediaStreaming() {
+    navigator.mediaDevices.getUserMedia({ video: true, audio: true}).then(stream => {
+        
+        b()
+        console.log("mediaStreaming called")
+        myStream = stream
+        
+        const userVideo = document.createElement('video')
+        userVideo.muted = true
+        addVideoStream(userVideo, stream)
+
+    })
+}
+
+function createVideo() { return document.createElement('video') }
+function addToGrid(element) { videoGrid.appendChild(element) }
+
+function a(msg) {console.log(msg)}
+function b() {console.log(Date.now() % 10000)}
+
+function addVideoStream(video, stream) {
+    video.addEventListener('loadedmetadata', () => video.play())
+    video.srcObject = stream
+    addToGrid(video)
+}
--- server.js
+++ server.js
@@ -1,15 +1,31 @@
+// Dependencies
 const express = require('express')
 const app = express()
 const server = require('http').Server(app)
-const io = require('socket.io').server
+const io = require('socket.io')(server)
 const { v4: generateID } = require('uuid')
 
-app.set('view engine', 'ejs')
 
-app.get('/', (req, res) => {
-    res.render('room.ejs')
+// App settings
+app.set('view engine', 'ejs')
+app.use(express.static(__dirname + "/public"))
+
+
+// Routes
+app.get('/', (req, res) => {res.redirect(`/${generateID()}`)})
+app.get('/:id', (req, res) => {res.render('room.ejs', { roomId: req.params.id })})
+
+
+// On socket connection with a user
+io.on('connection', socket => {
+    socket.emit('welcome', 'Hi new user')
+    socket.on('join-room', (roomId, connectedUserId) => {
+        socket.join(roomId)
+        socket.to(roomId).emit('user-connected', connectedUserId)
+        socket.on('disconnect', () => {socket.to(roomId).emit('user-diconnected', connectedUserId)})
+    })
 })
 
-console.log('testing')
 
+// Create a server that listens on for 
 server.listen(process.env.PORT || 3000)
